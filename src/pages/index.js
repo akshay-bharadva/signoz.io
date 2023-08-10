7aae8b02c984243b05a75eaf4299ab08dd9b97e9
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-08-10 23:44:10 +0530

    feat:placeholder content structure for new landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import ReactModal from "react-modal";
 import { Header } from "../modules/index-header";
 import { TrustedByTeams } from "../modules/trusted-by";
 import { WhyOpenTelemetry } from "../modules/why-opentelemetry";
-import { DataProtection } from "../modules/data-protection";
 import { SigNozFeatures } from "../modules/index-features";
 import { Testimonials } from "../modules/testimonials";
 
@@ -280,78 +279,18 @@ function Home() {
 
         <main className="landing-section">
           <Header />
-
           <TrustedByTeams />
-
           <SigNozFeatures />
-
           <WhyOpenTelemetry />
-
+          <LatestInOpenTelementry />
+          <BuildForDevelopers />
+          <DataProtection />
+          <Observability />
+          <Pricing />
           <Statistics />
-
           <Testimonials />
-
           <Tutorials />
-
-          <section>
-            <div
-              className="container"
-              style={{ marginTop: "6rem", marginBottom: "3rem" }}
-            >
-              <div class="row">
-                <div class="col col--4">
-                  <p className="faq_left_panel text--center margin--md">
-                    Open source and Free to self-host{" "}
-                  </p>
-                </div>
-
-                <div class="col col--8">
-                  <p className="hero__subtitle margin--md">
-                    Frequently Asked Questions
-                  </p>
-                  <div class="card-demo margin--md">
-                    <FAQBody />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-
-          <section>
-            <div className="container" style={{ marginBottom: "2rem" }}>
-              <div class="row">
-                <div class="col col--6">
-                  <p className="bottom_cta_interested text--center margin--md">
-                    Interested in trying out SigNoz?{" "}
-                  </p>
-                </div>
-                <div class="col col--6">
-                  <div>
-                    <Link
-                      className="button button--primary margin--md "
-                      href={"/teams/"}
-                      id="btn-get-started-homepage-bottom"
-                    >
-                      Try SigNoz Cloud
-                    </Link>
-                    <Link
-                      style={{
-                        margin: "6px",
-                        paddingLeft: "30px",
-                        paddingRight: "30px",
-                      }}
-                      className="button button--outline button--secondary "
-                      href={"/docs/install/"}
-                      onClick={requestDemoClicked}
-                      id="btn-self-hosted-homepage-bottom"
-                    >
-                      Self Host
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
+          <CTA />
         </main>
       </Layout>
       <div id={"modal-root"}></div>
@@ -401,20 +340,20 @@ const Statistics = () => {
   );
 };
 
-const Tutorials = () => {
+const LatestInOpenTelementry = () => {
   return (
     <section>
       <div
         className="container"
         style={{ marginTop: "6rem", marginBottom: "2rem" }}
       >
-        <p>LEARN</p>
-        <h1 class="text--center ">Tutorials</h1>
-        <div class="row">
-          <div class="col col--4">
-            <div class="card-demo margin--sm">
-              <div class="card rounded-none">
-                <div class="card__body p-0">
+        <p>READ ABOUT</p>
+        <h1 className="text--center ">Latest in OpenTelementry</h1>
+        <div className="row">
+          <div className="col col--4">
+            <div className="card-demo margin--sm">
+              <div className="card rounded-none">
+                <div className="card__body p-0">
                   <div className="flex flex-col gap-5">
                     <LiteYoutubeEmbed id="oQFMfEc9JNI" mute={false} />
                     <p className="px-5">
@@ -426,10 +365,10 @@ const Tutorials = () => {
               </div>
             </div>
           </div>
-          <div class="col col--4">
-            <div class="card-demo margin--sm">
-              <div class="card rounded-none">
-                <div class="card__body p-0">
+          <div className="col col--4">
+            <div className="card-demo margin--sm">
+              <div className="card rounded-none">
+                <div className="card__body p-0">
                   <div className="flex flex-col gap-5">
                     <LiteYoutubeEmbed id="u2PiWKEdjCw" mute={false} />
                     <p className="px-5">
@@ -441,10 +380,10 @@ const Tutorials = () => {
               </div>
             </div>
           </div>
-          <div class="col col--4">
-            <div class="card-demo margin--sm">
-              <div class="card rounded-none">
-                <div class="card__body p-0">
+          <div className="col col--4">
+            <div className="card-demo margin--sm">
+              <div className="card rounded-none">
+                <div className="card__body p-0">
                   <div className="flex flex-col gap-5">
                     <LiteYoutubeEmbed id="CgByZJeuRZY" mute={false} />
                     <p className="px-5">
@@ -456,6 +395,268 @@ const Tutorials = () => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Tutorials = () => {
+  return (
+    <section>
+      <div
+        className="container"
+        style={{ marginTop: "6rem", marginBottom: "2rem" }}
+      >
+        <p>LEARN</p>
+        <h1 className="text--center ">Tutorials</h1>
+        <div className="row">
+          <div className="col col--4">
+            <div className="card-demo margin--sm">
+              <div className="card rounded-none">
+                <div className="card__body p-0">
+                  <div className="flex flex-col gap-5">
+                    <LiteYoutubeEmbed id="oQFMfEc9JNI" mute={false} />
+                    <p className="px-5">
+                      Using an open source standard frees you from vendor
+                      lock-in.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col col--4">
+            <div className="card-demo margin--sm">
+              <div className="card rounded-none">
+                <div className="card__body p-0">
+                  <div className="flex flex-col gap-5">
+                    <LiteYoutubeEmbed id="u2PiWKEdjCw" mute={false} />
+                    <p className="px-5">
+                      Using an open source standard frees you from vendor
+                      lock-in.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col col--4">
+            <div className="card-demo margin--sm">
+              <div className="card rounded-none">
+                <div className="card__body p-0">
+                  <div className="flex flex-col gap-5">
+                    <LiteYoutubeEmbed id="CgByZJeuRZY" mute={false} />
+                    <p className="px-5">
+                      Using an open source standard frees you from vendor
+                      lock-in.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const BuildForDevelopers = () => {
+  const REASONS = [
+    {
+      title: "Query Builder",
+      desc: "Write queries on all telemetry signals. Run aggregates, and apply filters and formulas to get deeper insights from your data.",
+      figure: "",
+    },
+    {
+      title: "Columnar Database",
+      desc: "SigNoz uses ClickHouse - a fast open source distributed columnar database. Ingestion and aggregations are lightening fast.",
+      figure: "",
+    },
+    {
+      title: "Data Pipelines",
+      desc: "Build data pipelines easily with SigNoz OTel Collector. Integrate any existing pipeline with OTel Collector to send data to SigNoz.",
+      figure: "",
+    },
+    {
+      title: "Source Code",
+      desc: "Check out the entire source code of SigNoz on GitHub. Create issues, build features & integrations, get started without contacting any sales rep.",
+      figure: "",
+    },
+  ];
+  return (
+    <section class="overflow-hidden">
+      <div class="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-8">
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            Get granular control over your observability data.
+          </p>
+          <h1 className="sm:text-3xl text-2xl font-medium mb-4">
+            Built for developers like you.
+          </h1>
+        </div>
+        <div class="-my-8 divide-y-2 divide-gray-100 max-w-3xl mx-auto">
+          {REASONS.map((reason) => (
+            <div class="pb-8 flex flex-wrap md:flex-nowrap" key={reason.title}>
+              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span class="font-semibold title-font text-gray-700">
+                  CATEGORY
+                </span>
+              </div>
+              <div class="md:flex-grow">
+                <h2 class="text-2xl font-medium mb-2">{reason.title}</h2>
+                <p class="leading-relaxed">{reason.desc}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Pricing = () => {
+  return (
+    <section className="">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full">
+          <h1 className="sm:text-3xl text-2xl font-medium mb-4">
+            Pricing you can trust.
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            Tired of Datadog’s unpredictable bills or New Relic’s user-based
+            pricing? We’re here for you.
+          </p>
+        </div>
+        <div className="flex flex-wrap">
+          <div className="xl:w-1/3 lg:w-1/3 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+            <h2 className="text-lg sm:text-xl mb-2">No user-based pricing</h2>
+            <p className="leading-relaxed text-base mb-4">
+              Add as many team members as you want.
+            </p>
+          </div>
+          <div className="xl:w-1/3 lg:w-1/3 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+            <h2 className="text-lg sm:text-xl mb-2">
+              Simple usage-based pricing
+            </h2>
+            <p className="leading-relaxed text-base mb-4">
+              Only pay for the data you send.
+            </p>
+          </div>
+          <div className="xl:w-1/3 lg:w-1/3 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+            <h2 className="text-lg sm:text-xl mb-2">
+              No special pricing for custom metrics
+            </h2>
+            <p className="leading-relaxed text-base mb-4">
+              All metrics charged simply at $0.1 per million samples.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const CTA = () => {
+  return (
+    <section>
+      <div className="container" style={{ marginBottom: "2rem" }}>
+        <div className="">
+          <div className="">
+            <p>
+              OpenTelemetry-Native Metrics, Logs, and Traces in a single pane of
+              glass
+            </p>
+            <p>Check out our hosted and enterprise solutions.</p>
+          </div>
+          <div className="">
+            <div>
+              <Link
+                className="button button--primary  "
+                href={"/teams/"}
+                id="btn-get-started-homepage-bottom"
+              >
+                Try SigNoz Cloud
+              </Link>
+              <Link
+                style={{
+                  margin: "6px",
+                  paddingLeft: "30px",
+                  paddingRight: "30px",
+                }}
+                className="button button--outline button--secondary "
+                href={"/docs/install/"}
+                onClick={requestDemoClicked}
+                id="btn-self-hosted-homepage-bottom"
+              >
+                Self Host
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Observability = () => {
+  return (
+    <section>
+      <div className="flex flex-col justify-center items-center">
+        <p>Enterprise Grade Observability</p>
+        <h2>
+          Get access to observability at any scale with advanced security and
+          compliance.
+        </h2>
+      </div>
+      <div className="bg-gray-500 max-w-2xl p-5 grid grid-cols-2 mx-auto">
+        <div>
+          <ul>
+            <li>SSO and SAML support</li>
+            <li>Query API Keys</li>
+            <li>Advanced Security</li>
+            <li>AWS Private Link</li>
+          </ul>
+        </div>
+        <div>
+          <div className="flex flex-col justify-between">
+            <ul>
+              <li>VPC Peering</li>
+              <li>Custom Integrations</li>
+            </ul>
+            <button>Check Plans</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const DataProtection = () => {
+  return (
+    <section class="">
+      <div class="container flex flex-wrap px-5 py-24 mx-auto items-center">
+        <h2>Worried about data protection laws? We can help.</h2>
+        <div class="md:w-1/2 md:pr-12 md:py-8 md:border-r md:border-b-0 mb-10 md:mb-0 pb-10 border-b border-gray-200">
+          <p>For SigNoz Cloud</p>
+          <h3>Send data to your preferred hosting location</h3>
+          <p>
+            Store your data in the US, EU or India region depending on your
+            needs.
+          </p>
+          <Link>Try SigNoz Cloud</Link>
+        </div>
+        <div class="flex flex-col md:w-1/2 md:pl-12">
+          <p>For Self-Hosted</p>
+          <h3>Have your customer data in your infra</h3>
+          <p>
+            You can self-host SigNoz or opt for our managed self-hosted
+            offerings to have complete adherence to data privacy and regulation
+            laws.
+          </p>
+          <Link>Self Host</Link>
+          <Link>Managed by SigNoz in your cloud </Link>
         </div>
       </div>
     </section>
